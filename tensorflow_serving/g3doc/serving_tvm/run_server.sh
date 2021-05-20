2045b7a47db88596e8f4b0a2d39c0578c7289c02
lionthu <lionthu@tencent.com>
2021-05-20 04:05:25 +0000

    refine test scripts

--- tensorflow_serving/g3doc/serving_tvm/run_server.sh
+++ tensorflow_serving/g3doc/serving_tvm/run_server.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-./tensorflow_model_server --port=6003 --rest_api_port=6004 --model_config_file=/mnt/serving/tensorflow_serving/g3doc/serving_tvm/model_config.txt
+./tensorflow_model_server --port=6003 --rest_api_port=6004 --model_config_file=/root/serving/tensorflow_serving/g3doc/serving_tvm/model_config.txt
