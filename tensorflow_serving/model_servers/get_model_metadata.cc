dd831b3cc8e7cb786e5adc48016bdb7eb7c3fdad
huting <lionthu@tencent.com>
2021-07-28 10:22:06 +0800

    fix multiple definition error

---
+++ tensorflow_serving/model_servers/get_model_metadata.cc
@@ -0,0 +1,33 @@
+#include "tensorflow_serving/model_servers/get_model_status_impl.h"
+
+namespace tensorflow {
+namespace serving {
+
+Status BaseGetModelMetadata::ValidateGetModelMetadataRequest(const GetModelMetadataRequest& request) {
+  if (request.metadata_field_size() == 0) {
+    return tensorflow::Status(
+        tensorflow::error::INVALID_ARGUMENT,
+        "GetModelMetadataRequest must specify at least one metadata_field");
+  }
+  for (const auto& metadata_field : request.metadata_field()) {
+    if (metadata_field != BaseGetModelMetadata::kSignatureDef) {
+      return tensorflow::errors::InvalidArgument(
+          "Metadata field ", metadata_field, " is not supported");
+    }
+  }
+  return tensorflow::Status::OK();
+}
+
+Status BaseGetModelMetadata::GetModelMetadata(
+    ServerCore* core, const GetModelMetadataRequest& request,
+    GetModelMetadataResponse* response) {
+  if (!request.has_model_spec()) {
+    return tensorflow::Status(tensorflow::error::INVALID_ARGUMENT,
+                              "Missing ModelSpec");
+  }
+  return GetModelMetadataWithModelSpec(core, request.model_spec(), request,
+                                       response);
+}
+
+}
+}
--- tensorflow_serving/model_servers/get_model_metadata.h
+++ tensorflow_serving/model_servers/get_model_metadata.h
@@ -41,32 +41,6 @@ class BaseGetModelMetadata {
       GetModelMetadataResponse* response) {};
 };
 
-Status BaseGetModelMetadata::ValidateGetModelMetadataRequest(const GetModelMetadataRequest& request) {
-  if (request.metadata_field_size() == 0) {
-    return tensorflow::Status(
-        tensorflow::error::INVALID_ARGUMENT,
-        "GetModelMetadataRequest must specify at least one metadata_field");
-  }
-  for (const auto& metadata_field : request.metadata_field()) {
-    if (metadata_field != BaseGetModelMetadata::kSignatureDef) {
-      return tensorflow::errors::InvalidArgument(
-          "Metadata field ", metadata_field, " is not supported");
-    }
-  }
-  return tensorflow::Status::OK();
-}
-
-Status BaseGetModelMetadata::GetModelMetadata(
-    ServerCore* core, const GetModelMetadataRequest& request,
-    GetModelMetadataResponse* response) {
-  if (!request.has_model_spec()) {
-    return tensorflow::Status(tensorflow::error::INVALID_ARGUMENT,
-                              "Missing ModelSpec");
-  }
-  return GetModelMetadataWithModelSpec(core, request.model_spec(), request,
-                                       response);
-}
-
 }  // namespace serving
 }  // namespace tensorflow
 
